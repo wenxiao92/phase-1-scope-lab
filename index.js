02408a3320e3a15d1c512ba1691e9a0a9ad8492b
Wen Xiao <wennstyle@gmail.com>
2021-09-11 21:50:29 -0400

    commit tree ec483b90214aede833f74d05fbe441d2b036b935

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+var bestCustomer
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = 'Joe'
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "Martin";
+}
